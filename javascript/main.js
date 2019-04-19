f50551a984ecdf7f02f72eff0a2987fa8bdb0a47
Isaac de Souza <isaacde2012@gmail.com>
2019-04-19 12:28:09 -0300

    the dice game is ready to be played, i guess!

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+//separando os elementos
+
+let images = document.querySelectorAll("img");
+
+let header = document.querySelector("h1");
+
+
+//obtendo o número dos dados
+
+let player1 = Math.floor(Math.random()*6 + 1);
+
+let player2 = Math.floor(Math.random()*6 + 1);
+
+
+//mudando a imagem
+
+images[0].setAttribute("src","images/dice" + player1.toString() + ".png");
+
+images[1].setAttribute("src","images/dice" + player2.toString() + ".png");
+
+if(player1 > player2){
+    header.innerHTML = "<img src='images/red-flag.png'> Player 1 Wins!";
+}else if( player1 == player2){
+    header.textContent = "Draw!";
+}else
+    header.innerHTML = "Player 2 Wins! <img src='images/red-flag.png'> ";
